bcfeb4d032942db5159c0a647bf332c5387f194f
valery <valery.beretar@mail.ru>
2024-02-25 01:21:57 +0300

    submitData: Создание проекта и подключение к переменным окружения

--- camping/camping/settings.py
+++ camping/camping/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5!04(uni-*fl%a(u(3m92fxxa$wp)8g)&gz*qwb@%o8*p^#kug'
+SECRET_KEY = os.getenv('PAS_RED')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
